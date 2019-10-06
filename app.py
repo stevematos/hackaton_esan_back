24e99525f054830cdc8cb86fb916521cb1e4872b
Steve <stevematos1998>
2019-10-05 22:52:37 -0500

    nuevo cambio v6

--- app.py
+++ app.py
@@ -2,10 +2,6 @@
 
 from mvc import create_app
 
-def run(**kwargs):
-    main = create_app()
-    main.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-    main.run(host="0.0.0.0", debug=True)
-
-if __name__ == "__main__":
-    run()
+main = create_app()
+main.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+main.run(host="0.0.0.0", debug=True)
