cfe40498ef4b80acabece1672821153e2903303d
Steve <stevematos1998>
2019-10-05 22:56:34 -0500

    nuevo cambio v7

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 from mvc import create_app
 
-main = create_app()
-main.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-main.run(host="0.0.0.0", debug=True)
+if __name__ == "__main__":
+    main = create_app()
+    main.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+    main.run(host="0.0.0.0", debug=True)
