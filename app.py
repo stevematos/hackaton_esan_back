b7e145426890a8180faac323c888dbe3d764271f
Steve <stevematos1998>
2019-10-05 22:45:18 -0500

    nuevo cambio v3

--- app.py
+++ app.py
@@ -2,7 +2,10 @@
 
 from mvc import create_app
 
-if __name__ == "__main__":
+def run():
     main = create_app()
     main.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
     main.run(host="0.0.0.0", debug=True)
+
+if __name__ == "__main__":
+    run()
