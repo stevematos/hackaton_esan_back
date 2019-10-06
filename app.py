a156815f4d376f7a7fce4bcbd570e5671f0bb901
Steve <stevematos1998>
2019-10-05 23:15:43 -0500

    nuevo cambio v14

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 # compose_flask/app.py
 
 from mvc import create_app
+import os
 
 if __name__ == "__main__":
     app = create_app()
 
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-    app.run(host="0.0.0.0", debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0",port=port,debug=True)
